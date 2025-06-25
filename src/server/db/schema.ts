aaa7c3d167a15627d51c22c2417ed970a79dbb53
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2025-06-24 19:12:45 -0700

    sandbox is adding data to new file and folders table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,30 +1,43 @@
 import {
   int,
   text,
-  singlestoreTable,
+  bigint,
   index,
+  singlestoreTableCreator,
 } from "drizzle-orm/singlestore-core";
 
-export const files = singlestoreTable(
+export const createTable = singlestoreTableCreator(
+  (name) => `drive-tutorial_${name}`,
+);
+
+export const files = createTable(
   "files_table",
   {
-    id: int("id").primaryKey().autoincrement(),
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
     name: text("name").notNull(),
-    size: int("size").notNull(),
+    size: bigint("size", { mode: "number", unsigned: true }).notNull(),
     url: text("url").notNull(),
-    parent: int("parent").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+    // at some point we will add a column for the user id
+    // userId: int("user_id").notNull(),
   },
   (t) => {
     return [index("parent_index").on(t.parent)];
   },
 );
 
-export const folders = singlestoreTable(
+export const folders = createTable(
   "folders_table",
   {
-    id: int("id").primaryKey().autoincrement(),
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
     name: text("name").notNull(),
-    parent: int("parent"),
+    parent: bigint("parent", { mode: "number", unsigned: true }),
+    // at some point we will add a column for the user id
+    // userId: int("user_id").notNull(),
   },
   (t) => {
     return [index("parent_index").on(t.parent)];
