6bcb0bd52b3d64643f98dcf6f1ba57e4b2cf5b72
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2025-06-24 18:37:35 -0700

    schema update

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,12 +1,32 @@
 import {
   int,
-  bigint,
   text,
   singlestoreTable,
+  index,
 } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+export const files = singlestoreTable(
+  "files_table",
+  {
+    id: int("id").primaryKey().autoincrement(),
+    name: text("name").notNull(),
+    size: int("size").notNull(),
+    url: text("url").notNull(),
+    parent: int("parent").notNull(),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
+
+export const folders = singlestoreTable(
+  "folders_table",
+  {
+    id: int("id").primaryKey().autoincrement(),
+    name: text("name").notNull(),
+    parent: int("parent"),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
